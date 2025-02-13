505f727dd671ca14fa00a588615f1777e6fadb77
Khanon Curtis <63666204+khanCurtis@users.noreply.github.com>
2025-02-13 13:32:04 -0700

    Create loopEndings.java

---
+++ Java 2/loopEndings.java
@@ -0,0 +1,21 @@
+/*
+Loop Endings -- break & continue
+Khanon Curtis
+2/6/25
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            
+            System.out.println(i);
+        }
+
+
+
+    }
+}
