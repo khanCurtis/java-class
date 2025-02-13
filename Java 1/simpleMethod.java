a79fb1510bdaba9363e97df6bd591f4fc69b1da7
Khanon Curtis <63666204+khanCurtis@users.noreply.github.com>
2025-02-13 13:28:05 -0700

    Create simpleMethod.java

---
+++ Java 1/simpleMethod.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+simpleMethod
+Khanon Curtis
+5/17/24
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		printOne();
+		printTwo();
+		printThree();
+	}
+	public static void printOne(){
+	    System.out.println("Hello world!");
+	}
+	public static void printTwo(){
+	    System.out.println("Hello Mrs. Johnson!");
+	}
+	public static void printThree(){
+	    System.out.println("Goodbye world!");
+	}
+}
