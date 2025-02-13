9a3872b7729c3260a4ebb71b5ec94c54a57d3ef4
Khanon Curtis <63666204+khanCurtis@users.noreply.github.com>
2025-02-13 13:19:13 -0700

    Create printTable.java

---
+++ Java 1/printTable.java
@@ -0,0 +1,13 @@
+/******************************************************************************
+
+printTable (casting)
+Khanon Curtis
+2/27/24
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.printf("a \tb \tpow(a,b) \n1 \t2 \t%d \n2 \t3 \t%d \n3 \t4 \t%d \n4 \t5 \t%d \n5 \t6 \t%d", (int)Math.pow(1,2), (int)Math.pow(2,3), (int)Math.pow(3,4), (int)Math.pow(4,5), (int)Math.pow(5,6));
+	}
+}
