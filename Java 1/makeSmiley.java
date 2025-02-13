7f3c2c51ccb7ef1abc2d364d3014d9f01443955f
Khanon Curtis <63666204+khanCurtis@users.noreply.github.com>
2025-02-13 13:27:23 -0700

    Create makeSmiley.java

---
+++ Java 1/makeSmiley.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Methods Proj1: makeSmiley
+Khanon Curtis
+5/17/24
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		makeSmiley();
+		
+	}
+	
+	public static void makeSmiley(){
+	    
+
+	System.out.println("                 ooooooooooooooooooooo");
+	System.out.println("               ooooooooooooooooooooooooo");
+	System.out.println("              ooooooooooooooooooooooooooo");
+	System.out.println("             ooooooooooooooooooooooooooooo");
+	System.out.println("            oooooooooo  ooooooo  oooooooooo");
+	System.out.println("           oooooooooo    ooooo    oooooooooo");
+	System.out.println("          oooooooooo   $  ooo   $  oooooooooo");
+	System.out.println("         oooooooooooo    ooooo    oooooooooooo");
+	System.out.println("        oooooooooooooo  ooooooo  oooooooooooooo");
+	System.out.println("        ooooooooooooooooooooooooooooooooooooooo");
+	System.out.println("        oooo       ooooooooooooooooo       oooo");
+	System.out.println("         ooooo   ooooooooooooooooooooo   ooooo");
+	System.out.println("          ooooo   ooooooooooooooooooo   ooooo");
+	System.out.println("           ooooo   ooooooooooooooooo   ooooo");
+	System.out.println("            ooooo   ooooooooooooooo   ooooo");
+	System.out.println("             ooooo   ooooooooooooo   ooooo");
+	System.out.println("              ooooo                 ooooo");
+	System.out.println("               ooooo               ooooo");
+	System.out.println("                ooooooooooooooooooooooo");
+	System.out.println("                 ooooooooooooooooooooo");
+	
+	}	
+}
