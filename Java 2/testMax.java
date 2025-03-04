8e24349a43cbf51bedee63dbff4befa1063290fe
Khanon Curtis <63666204+khanCurtis@users.noreply.github.com>
2025-03-04 09:37:56 -0700

    Create testMax.java

---
+++ Java 2/testMax.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+testMax
+Khanon Curtis
+3/3/24
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+    public static int max(int num1, int num2) {
+        
+    }
+    
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter two whole numbers: ");
+		int num1 = input.nextInt();
+		int num2 = input.nextInt();
+		
+	}
+}
