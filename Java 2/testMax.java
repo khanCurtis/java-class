be4eaedf21684a28d64fa354b15cc955f18c34c1
Khanon Curtis <63666204+khanCurtis@users.noreply.github.com>
2025-03-05 13:23:13 -0700

    Update testMax.java

--- Java 2/testMax.java
+++ Java 2/testMax.java
@@ -8,16 +8,19 @@ Khanon Curtis
 import java.util.Scanner;
 public class Main
 {
-    public static int max(int num1, int num2) {
-        
+    
+    public static int max(int x, int y) {
+        return Math.max(x,y);
     }
     
 	public static void main(String[] args) {
 	    Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter two whole numbers: ");
-		int num1 = input.nextInt();
-		int num2 = input.nextInt();
+		int x = input.nextInt();
+		int y = input.nextInt();
+		
+		System.out.printf("%nThe maximum of %d and %d is %d", x, y, max(x, y));
 		
 	}
 }
