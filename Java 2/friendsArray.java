f7a371b7d21522f9003498149bedc8ac59d88b80
Khanon Curtis <63666204+khanCurtis@users.noreply.github.com>
2025-03-17 19:19:42 +0000

    Create friendsArray.java

---
+++ Java 2/friendsArray.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Friends Array
+Khanon Curtis
+3/17/25
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		
+		String [] friends = new String [6];
+		friends[0] = "Cindy";
+		friends[1] = "Gayle";
+		friends[2] = "David";
+		friends[3] = "Ivan";
+		friends[4] = "Esther";
+		friends[5] = "Laura";
+		
+		System.out.println("friend[0] : " + friends[0]);
+		System.out.println("friend[1] : " + friends[1]);
+		System.out.println("friend[2] : " + friends[2]);
+		System.out.println("friend[3] : " + friends[3]);
+		System.out.println("friend[4] : " + friends[4]);
+		System.out.println("friend[5] : " + friends[5]);
+	}
+}
