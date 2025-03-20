6f36dc60e6d09c186242fb38acd882e367628a90
khanCurtis <khanon.boyd@gmail.com>
2025-03-20 10:25:32 -0600

    commit via shell

---
+++ Java 2/pswdCreator.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+pswdCreator
+Khanon Curtis
+3/19/25
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print(Enter your birth city: );
+		String city = input.nextLine();
+		System.out.print(Enter your favorite color: );
+		String col = input.nextLine();
+		System.out.print(Enter a 2-digit number: );
+		String num = input.nextLine();
+		
+		//psw = first 3 letters of col + letters 2, 3, 4 of city + 2 dig num + rhs
+		String psw = 
+		
+	}
+}
+
