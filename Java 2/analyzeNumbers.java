77d11c3f3498426d431e6336f7659ed0e403d637
Khanon Curtis <63666204+khanCurtis@users.noreply.github.com>
2025-04-21 18:43:43 +0000

    Create analyzeNumbers.java

---
+++ Java 2/analyzeNumbers.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+analyzeNumbers
+Khanon Curtis
+4/9/25
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		input Scanner = new Scanner(System.in);
+		
+		System.out.print("Enter the number of items: ");
+		int num = input.nextInt();
+	}
+}
