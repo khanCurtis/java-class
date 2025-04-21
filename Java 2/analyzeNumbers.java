586a7a87ef21fc9973dbbe7ea246f7cba029b3f1
Khanon Curtis <63666204+khanCurtis@users.noreply.github.com>
2025-04-21 19:02:10 +0000

    Update analyzeNumbers.java

--- Java 2/analyzeNumbers.java
+++ Java 2/analyzeNumbers.java
@@ -6,12 +6,34 @@ Khanon Curtis
 
 *******************************************************************************/
 import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args) {
-		input Scanner = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter the number of items: ");
-		int num = input.nextInt();
+		int numItems = input.nextInt();
+		double[] numbers = new double[numItems];
+		double total = 0;
+		
+		System.out.println("\nEnter the numbers: ");
+		for (int i = 0; i < numItems; i++) {
+		    numbers[i] = input.nextDouble();
+		    total += numbers[i];
+		}
+		
+		double average = total / numItems;
+		System.out.printf("\nAverage: %.1f\n", average);
+		
+		int count = 0;
+		for (double num : numbers) {
+		    if (num > average) {
+		        count++;
+		    }
+		}
+		
+		System.out.println("Number of elements greater than average: " + count);
+		
 	}
 }
