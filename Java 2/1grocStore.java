40eac7257b394c782d9f10765d6cdb5a89a5107a
Khanon Curtis <63666204+khanCurtis@users.noreply.github.com>
2025-04-21 18:44:54 +0000

    Create 1grocStore.java

---
+++ Java 2/1grocStore.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+1grocStore
+Khanon Curtis
+4/21/25
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		groceryStore houstonStore = new groceryStore();
+		groceryStore seattleStore = new groceryStore();
+		groceryStore orlandoStore = new groceryStore();
+	}
+}
+
+class groceryStore {
+    int appleSales;
+    int applePrice;
+    int orangeSales;
+    int orangePrice;
+}
+    
